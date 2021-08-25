085cc6ea963fe272f174868a2c64f5b8140bb961
minskyyy <74529247+minskyyy@users.noreply.github.com>
2021-08-26 00:25:01 +0200

    password encryption

--- authenticate.php
+++ authenticate.php
@@ -32,7 +32,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
   	$stmt->fetch();
   	// Account exists, now we verify the password.
   	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-  	if ($_POST['password'] === $password) {
+  	if (password_verify($_POST['password'], $password)) {
   		// Verification success! User has logged-in!
   		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
   		session_regenerate_id();
