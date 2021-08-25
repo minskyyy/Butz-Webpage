044a1c7ceb8b1be53dbb10e106c237a9b8591f88
minskyyy <74529247+minskyyy@users.noreply.github.com>
2021-08-26 00:20:19 +0200

    home screen for login

--- authenticate.php
+++ authenticate.php
@@ -32,14 +32,14 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
   	$stmt->fetch();
   	// Account exists, now we verify the password.
   	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-  	if (password_verify($_POST['password'], $password)) {
+  	if ($_POST['password'] === $password) {
   		// Verification success! User has logged-in!
   		// Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
   		session_regenerate_id();
   		$_SESSION['loggedin'] = TRUE;
   		$_SESSION['name'] = $_POST['username'];
   		$_SESSION['id'] = $id;
-  		echo 'Welcome ' . $_SESSION['name'] . '!';
+  		header('Location: home.php');
   	} else {
   		// Incorrect password
   		echo 'Incorrect password!';
