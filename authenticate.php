c1bfdf829e5c030a3dbc8f7d251bee231999a451
minskyyy <74529247+minskyyy@users.noreply.github.com>
2021-08-26 00:00:04 +0200

    athenticate php

--- authenticate.php
+++ authenticate.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'sql11.freemysqlhosting.net';
-$DATABASE_USER = 'sql11432828';
-$DATABASE_PASS = 'aZHb8zgbIW';
-$DATABASE_NAME = 'sql11432828';
+$DATABASE_HOST = 'db5004631967.hosting-data.io';
+$DATABASE_USER = 'dbu2118458';
+$DATABASE_PASS = 'ZKW5WjPbkgwauB2j';
+$DATABASE_NAME = 'dbs3876923';
+
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
