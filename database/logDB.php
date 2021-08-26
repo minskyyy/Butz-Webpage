d55f12b97b5c637aba24aae6645bdd337739423d
minskyyy <74529247+minskyyy@users.noreply.github.com>
2021-08-26 13:45:43 +0200

    newsfeed

---
+++ database/logDB.php
@@ -0,0 +1,9 @@
+<?php
+// Change this to your connection info.
+$DATABASE_HOST = 'db5004631967.hosting-data.io';
+$DATABASE_USER = 'dbu2118458';
+$DATABASE_PASS = 'ZKW5WjPbkgwauB2j';
+$DATABASE_NAME = 'dbs3876923';
+
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
---
+++ index2.php
@@ -0,0 +1,37 @@
+<?php
+  include_once 'database/logDB.php';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ 	<head>
+ 		<meta charset="utf-8">
+ 		<title>Home Page</title>
+ 		<!-- Styles -->
+ 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+ 		<link href="css/bootstrap.min.css" rel="stylesheet">
+ 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ 		<link href="css/fontawesome-all.min.css" rel="stylesheet">
+ 		<link href="css/swiper.css" rel="stylesheet">
+ 		<link href="css/styles.css" rel="stylesheet">
+
+ 	</head>
+ 	<body class="loggedin">
+        <?php
+          $sql = "SELECT * FROM news;";
+          $result = mysqli_query($con, $sql);
+          $resultCheck = mysqli_num_rows($result);
+          if ($resultCheck > 0 ){
+            while($row = mysqli_fetch_assoc($result)){
+              echo $row['input'];
+            }
+          }
+         ?>
+
+ 		<!-- Scripts -->
+ 		<script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+ 		<script src="js/scripts.js"></script> <!-- Custom scripts -->
+
+ 	</body>
+
+ </html>
